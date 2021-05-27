f6894dd2e672ea57aed6e67ef034b45529be51b1
Ondřej Kocián <kocis.34@gmail.com>
2021-05-27 11:20:10 +0000

    add data

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+const data = require('@begin/data')
 const arc = require("@architect/functions");
+const {randomInt} = require("crypto")
 
 exports.handler = arc.http.async(async (req) => {
+  await data.set({
+    table: "data",
+    key: "cool",
+    stored: {
+      n: randomInt(100),
+      isCool: Math.random() > 0.5
+    }
+  })
+  const d = await data.get({
+    table: "data",
+    key: "cool"
+  })
   return {
     statusCode: 201,
     json: {
       ok: true,
+      ...d
     },
     cors: true,
   };
